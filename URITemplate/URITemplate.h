b415103885157c674d65ea432b0e2cf9e07287bd
Isnan Franseda <ifranseda@gmail.com>
2018-07-18 17:07:08 +0800

    Add Carthage support

---
+++ URITemplate/URITemplate.h
@@ -0,0 +1,19 @@
+//
+//  URITemplate.h
+//  URITemplate
+//
+//  Created by Isnan Franseda on 7/18/18.
+//  Copyright © 2018 Astro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for URITemplate.
+FOUNDATION_EXPORT double URITemplateVersionNumber;
+
+//! Project version string for URITemplate.
+FOUNDATION_EXPORT const unsigned char URITemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URITemplate/PublicHeader.h>
+
+
